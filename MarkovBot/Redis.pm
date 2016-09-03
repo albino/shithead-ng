7c34709885a92912177fa6600cb221cb87bdb1d5
Al Beano <albino@autistici.org>
2016-09-03 12:15:23 +0100

    redis() is no longer useless

--- MarkovBot/Redis.pm
+++ MarkovBot/Redis.pm
@@ -12,10 +12,6 @@ use lib $Bin;
 use MarkovBot::Config;
 
 sub redis() {
-  # Returns a redis object
-  # This is fairly useless but in the future could be used to
-  # pass specific options to redis.
-  #
   # TODO: support connection to redis via a socket
 
   return new Redis (
